f97092a81b9036cffefd0e6d8c3692afe1eaf006
David <davi5663@edu.sde.dk>
2020-08-03 09:25:26 +0200

    Added NumberCounter

--- main.cpp
+++ main.cpp
@@ -1,57 +1,72 @@
+#include "Grove_LCD_RGB_Backlight.h"
 #include "mbed.h"
 #include "stm32746g_discovery_lcd.h"
-#include "Grove_LCD_RGB_Backlight.h"
-
 
 DigitalOut myblueled(D3);
-DigitalOut myredled (D2);
-DigitalIn mybutton (D4);
-Grove_LCD_RGB_Backlight mylcd(D15, D14);
+DigitalOut myredled(D2);
+DigitalIn mybutton(D4);
+Grove_LCD_RGB_Backlight mylcd(D14, D15);
 /*DigitalOut mylightsensor (D2);*/
 /*DigitalOut mySensor(D2); */
+Thread t;    // Creates Thred
+int num = 0; // Declares the number
 
-int main() {  
-      
-        
-    mylcd.setRGB(0xff,0xff, 0xff);
-    BSP_LCD_Init();
-    BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
-    BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
-    BSP_LCD_Clear(LCD_COLOR_BLACK);
-    BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
-    BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
-    BSP_LCD_DisplayStringAt(0, 1, (uint8_t *)"DAVID", CENTER_MODE);
-    HAL_Delay(1000);
-    
-     while(1)
-    {    
-   
-        if (mybutton == 1)
-        {
-            BSP_LCD_Clear(LCD_COLOR_WHITE);
-            BSP_LCD_DisplayStringAt(0, LINE(4), (uint8_t *)"PRESSING BUTTON", CENTER_MODE);                     
-            myblueled = 1;
-            myredled = 1;
-            ThisThread::sleep_for(100); //Wait in miliseconds, this is 0.1 second
-            myblueled = 0;
-            myredled = 0;
-            ThisThread::sleep_for(50);
-            /*mylightsensor =1;*/
-            /*mySensor = 1; */
+void Numbercounter() {
+  while (1) {
+    char maxlength[20];                       // How long the character can be
+    sprintf(maxlength, "Number is: %d", num); //%d helps to print the integer
+    mylcd.clear();
+    mylcd.print(maxlength);
+    num++; // Counts up the number
+    ThisThread::sleep_for(1000);
+  }
+}
+
+int main() {
+  printf("program started...");
+
+  mylcd.setRGB(0xff, 0xff, 0xff);
+  // mylcd.print("We drive fast cars!");
+  BSP_LCD_Init();
+  BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
+  BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
+  BSP_LCD_Clear(LCD_COLOR_BLACK);
+  BSP_LCD_SetFont(&LCD_DEFAULT_FONT);
+  BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
+  BSP_LCD_DisplayStringAt(0, 1, (uint8_t *)"WELCOME TO MY PROJECT! MY NAME IS DAVID", CENTER_MODE);
+  HAL_Delay(1000);
+  t.start(&Numbercounter);
+  while (1) {
+    if (mybutton == 1) {
+      while (1) {
+
+        if (mybutton == 1) {
+          num = 0;
+          BSP_LCD_Clear(LCD_COLOR_WHITE);
+          BSP_LCD_DisplayStringAt(0, LINE(4), (uint8_t *)"PRESSING BUTTON",
+                                  CENTER_MODE);
+          myblueled = 1;
+          myredled = 1;
+          ThisThread::sleep_for(100); // Wait in miliseconds, this is 0.1 second
+          myblueled = 0;
+          myredled = 0;
+          ThisThread::sleep_for(50);
+          /*mylightsensor =1;*/
+          /*mySensor = 1; */
         }
-            
-            else if (mybutton == 0)
-            { 
-                myblueled = 0;
-                myredled = 0;
-                BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
-                BSP_LCD_ClearStringLine(4); //Clears string number 4 which is line 30
-                BSP_LCD_DisplayStringAt(0, 1, (uint8_t *)"NOT PRESSING THE BUTTON", CENTER_MODE);
-                /*mylightsensor = 0;*/
-                /* mySensor = 0;*/
-            }  
+
+        else if (mybutton == 0) {
+          myblueled = 0;
+          myredled = 0;
+          BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
+          BSP_LCD_SetTextColor(LCD_COLOR_BLUE);
+          BSP_LCD_DrawRect(BSP_LCD_GetXSize()/2, BSP_LCD_GetYSize()/2, 40, 50);
+          BSP_LCD_ClearStringLine(4); // Clears string number 4 which is line 30
+          BSP_LCD_DisplayStringAt(0, 1, (uint8_t *)"NOT PRESSING THE BUTTON",  CENTER_MODE);
+          /*mylightsensor = 0;*/
+          /* mySensor = 0;*/
+        }
+      }
     }
-        
-
-
+  }
 }
